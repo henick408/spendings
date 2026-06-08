4f9f9d26f48f56e8299a43e836254d36c33b6c34
henick408 <marcel.snopkowski@gmail.com>
2026-06-08 23:18:15 +0200

    Initial commit

---
+++ src/test/java/org/henick/spendings/SpendingsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.henick.spendings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpendingsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
